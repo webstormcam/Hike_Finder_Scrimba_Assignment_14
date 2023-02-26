12c8908390f9cf81eadc03c7a03f90e5f33f6e6f
Cameron Chandler <webstormcam@gmail.com>
2023-02-26 12:47:08 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,6 @@
 const ham = document.getElementById('hamburger')
 const mobileNav = document.getElementById('mobile-nav')
+// This will toggle the Nav options on small screens!
 ham.addEventListener('click',function(){
     mobileNav.classList.toggle('mobile-options')
 })
